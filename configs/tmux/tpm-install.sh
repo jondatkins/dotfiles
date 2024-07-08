8035c249bc33aa3d7375f3e09221106089518921
jondatkins <jon.david.atkins@gmail.com>
2024-07-08 18:33:43 +0100

    Tmux config should be much better now.

--- configs/tmux/tpm-install.sh
+++ configs/tmux/tpm-install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Install tmux package manager only if it isn't already installed, assumes that $TMUX_PLUGIN_MANAGER_PATH has been
 # set in tmux.conf
-if [ -d "$TMUX_PLUGIN_MANAGER_PATH" ]; then
-	echo "tpm is installed at $TMUX_PLUGIN_MANAGER_PATH"
+# if "test ! -d ~/.tmux/plugins/tpm" \
+#    "run 'git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins'"
+if [ ! -d "~/.tmux/plugins/tpm" ]; then
+	echo "tpm is installed"
 else
-	echo "installing tpm at $TMUX_PLUGIN_MANAGER_PATH"
-	git clone https://github.com/tmux-plugins/tpm $TMUX_PLUGIN_MANAGER_PATH
+	echo "installing tpm"
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins
 fi
