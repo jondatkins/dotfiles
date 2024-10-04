af164fb0f97e6d30831b9b1bb7626e907cbaff51
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-10-04 17:26:49 +0100

    More config for raspberry pi.

--- buildNeovimPi.sh
+++ buildNeovimPi.sh
@@ -6,3 +6,10 @@ make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build
 cpack -G DEB
 sudo dpkg -i nvim-linux64.deb
+
+sudo apt install ninja-build gettext cmake unzip curl
+git clone https://github.com/neovim/neovim
+cd neovim
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+get checkout stable
+sudo make install
