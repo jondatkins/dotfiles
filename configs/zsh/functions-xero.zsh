025efeb2e3ed4111267ea4ebc9858dcea980b4bc
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-11-15 13:11:00 +0000

    Alias 'zip' and 'unzip' where masking actual zip/unzip binaries.

--- configs/zsh/functions-xero.zsh
+++ configs/zsh/functions-xero.zsh
@@ -15,36 +15,36 @@ function t() {
 }
 
 #█▓▒░ {de,}compression
-unzip() {
-  if [[ -f $1 ]]; then
-    case $1 in
-      *.tar.bz2)  tar xvjf "$1" ;;
-      *.tar.gz)   tar xvzf "$1" ;;
-      *.bz2)       bunzip2 "$1" ;;
-      *.rar)       unrar x "$1" ;;
-      *.gz)         gunzip "$1" ;;
-      *.tar)       tar xvf "$1" ;;
-      *.tbz2)     tar xvjf "$1" ;;
-      *.tgz)      tar xvzf "$1" ;;
-      *.zip)         unzip "$1" ;;
-      *.Z)      uncompress "$1" ;;
-      *.7z)           7z x "$1" ;;
-      *.xz)           unxz "$1" ;;
-			*)             echo "'$1' unknown compression" ;;
-    esac
-  else
-    echo "'$1' not a valid file"
-  fi
-}
-
-zip() {
-	if [[ -f "$1" ||  -d "$1" ]]; then
-		tar zcvf "${1}_$(date '+%Y-%m-%d').tar.gz" "$1";
-	else
-		echo "not a valid file or dir"
-	fi
-}
-
+# unzip() {
+#   if [[ -f $1 ]]; then
+#     case $1 in
+#       *.tar.bz2)  tar xvjf "$1" ;;
+#       *.tar.gz)   tar xvzf "$1" ;;
+#       *.bz2)       bunzip2 "$1" ;;
+#       *.rar)       unrar x "$1" ;;
+#       *.gz)         gunzip "$1" ;;
+#       *.tar)       tar xvf "$1" ;;
+#       *.tbz2)     tar xvjf "$1" ;;
+#       *.tgz)      tar xvzf "$1" ;;
+#       *.zip)         unzip "$1" ;;
+#       *.Z)      uncompress "$1" ;;
+#       *.7z)           7z x "$1" ;;
+#       *.xz)           unxz "$1" ;;
+# 			*)             echo "'$1' unknown compression" ;;
+#     esac
+#   else
+#     echo "'$1' not a valid file"
+#   fi
+# }
+#
+# zip() {
+# 	if [[ -f "$1" ||  -d "$1" ]]; then
+# 		tar zcvf "${1}_$(date '+%Y-%m-%d').tar.gz" "$1";
+# 	else
+# 		echo "not a valid file or dir"
+# 	fi
+# }
+#
 #█▓▒░ disk info
 function disks() {
 	# echo
