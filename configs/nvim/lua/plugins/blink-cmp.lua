f12f84e06c5c07f142a03af3d3ffce3af55cfc01
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2024-12-21 21:18:37 +0000

    More markdown plugins.

--- configs/nvim/lua/plugins/blink-cmp.lua
+++ configs/nvim/lua/plugins/blink-cmp.lua
@@ -21,7 +21,41 @@ return {
       end,
     },
     sources = {
+      compat = { "luasnip" },
       default = { "lsp", "path", "luasnip", "buffer" },
+    },
+    -- Completions for checkboxes and callouts, as described at:
+    -- https://github.com/MeanderingProgrammer/render-markdown.nvim
+    completion = {
+      enabled_providers = { "lsp", "path", "snippets", "buffer", "markdown" },
+    },
+    providers = {
+      markdown = { name = "RenderMarkdown", module = "render-markdown.integ.blink" },
+      -- This is from linkarzu's dotfiles here
+      -- https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/plugins/blink-cmp.lua
+      luasnip = {
+        name = "luasnip",
+        enabled = true,
+        module = "blink.compat.source",
+        kind = "Snippet",
+        score_offset = 900, -- the higher the number, the higher the priority
+      },
+      lsp = {
+        name = "lsp",
+        enabled = true,
+        module = "blink.cmp.sources.lsp",
+        kind = "LSP",
+        score_offset = 1000, -- the higher the number, the higher the priority
+      },
+      -- Third class citizen mf always talking shit
+      copilot = {
+        name = "copilot",
+        enabled = true,
+        module = "blink-cmp-copilot",
+        kind = "Copilot",
+        score_offset = 0, -- the higher the number, the higher the priority
+        async = true,
+      },
     },
   },
 }
