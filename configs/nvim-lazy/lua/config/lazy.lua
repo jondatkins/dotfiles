885863b46c7a5823b83ee702aa4fcacd6523b1be
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2025-07-11 13:37:51 +0100

    Fixing Gruvbox config so I can have transparency. Adding lint.lua for markdown config.

--- configs/nvim-lazy/lua/config/lazy.lua
+++ configs/nvim-lazy/lua/config/lazy.lua
@@ -42,6 +42,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "gruvbox", "catpuccin", "tokyonight", "habamax" } },
+
   checker = { enabled = true, notify = false, frequency = 86400 }, -- automatically check for plugin updates
   performance = {
     rtp = {
