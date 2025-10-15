a82ebfcc81ca12ac11a6d36532eb162e7b171127
jondatkins <122027005+jondatkins@users.noreply.github.com>
2025-10-15 12:00:56 +0100

    New install script for omarchy. Adding package for krita / AI.

---
+++ ArchInstallScripts/installOmarchyApps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo pacman -S --needed --noconfirm firefox
+yay -S --needed --noconfirm ghostty
+sudo pacman -S --needed --noconfirm feh
+# sudo pacman -S --needed --noconfirm gimp gimp-plugin-gmic
+sudo pacman -S --needed --noconfirm krita krita-plugin-gmic
+sudo pacman -S python-pyqt5
+# play and rip CDs and DVDs
+sudo pacman -S --needed --noconfirm libcdio cdparanoia
+sudo pacman -S --needed --noconfirm strawberry sound-juicer
+sudo pacman -S --needed --noconfirm libdvdread libdvdcss libdvdnav
+yay -S --needed --noconfirm wacomtablet
+yay -S --needed --noconfirm dropbox
+sudo pacman -S webp-pixbuf-loader
