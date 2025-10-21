194731abeb06b75f3d5fe764f87f74267499ff1f
jondatkins <122027005+jondatkins@users.noreply.github.com>
2025-10-21 15:42:39 +0100

    Reverting to my original setup, minus any java / jdtls, and pruning some unused plugins.

--- configs/nvim-lazy/lua/plugins/example.lua
+++ configs/nvim-lazy/lua/plugins/example.lua
@@ -181,9 +181,10 @@ return {
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
+
   -- add any tools you want to have installed below
   {
-    "mason-org/mason.nvim",
+    "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
         "stylua",
--- configs/nvim-lazy/lua/plugins/nvim-java.lua
+++
@@ -1,4 +0,0 @@
-if true then
-  return {}
-end
-return { "nvim-java/nvim-java" }
