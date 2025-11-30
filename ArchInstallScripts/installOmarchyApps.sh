c1bfc9a83f724e0c7dcad9813e1c267b353f1215
jondatkins <122027005+jondatkins@users.noreply.github.com>
2025-11-30 20:32:34 +0000

    Adding zellij and kanata

--- ArchInstallScripts/installOmarchyApps.sh
+++ ArchInstallScripts/installOmarchyApps.sh
@@ -20,3 +20,5 @@ yay -S --needed --noconfirm tmux-sixel-git
 sudo pacman --needed --noconfirm -S openssh
 sudo systemctl start sshd
 sudo systemctl enable sshd
+yay -S --needed --noconfirm zellij
+yay -S --needed --noconfirm kanata
