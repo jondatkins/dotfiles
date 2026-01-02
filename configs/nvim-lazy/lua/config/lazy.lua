a27992c82864978015c816dc0500048e2ad7eea6
jondatkins <122027005+jondatkins@users.noreply.github.com>
2026-01-02 20:50:58 +0000

    Adding go and c to lazyvim.

--- configs/nvim-lazy/lua/config/lazy.lua
+++ configs/nvim-lazy/lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.clangd" },
+    -- { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
---
+++ configs/nvim-lazy/lua/plugins/conform.lua
@@ -0,0 +1,12 @@
+-- ~/.config/nvim/lua/plugins/conform.lua
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      c = { "clang_format" },
+      cpp = { "clang_format" },
+      objc = { "clang_format" },
+      objcpp = { "clang_format" },
+    },
+  },
+}
