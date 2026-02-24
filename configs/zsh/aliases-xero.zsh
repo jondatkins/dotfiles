f1e5d904582ce625da9661cfbccae74001f9c2f2
jondatkins <122027005+jondatkins@users.noreply.github.com>
2026-02-24 14:28:12 +0000

    Removing more aliases, e.g. 'mkdir -p'.

--- configs/zsh/aliases-xero.zsh
+++ configs/zsh/aliases-xero.zsh
@@ -62,19 +62,19 @@ alias gx="git reset --hard @"
 
 #overrides
 alias sudo="sudo "  # expand aliases with sudo
-alias mkdir="mkdir -p"
-alias cp="cp -r"
-alias scp="scp -r"
+# alias mkdir="mkdir -p"
+# alias cp="cp -r"
+# alias scp="scp -r"
 alias vimdiff="nvim -d --cmd ':lua vim.g.noplugins=1'"
 # alias apt="sudo apt"
 # alias pacman="sudo pacman"
 # alias doc="sudo docker"
 # alias docker="sudo docker"
 # alias systemctl="sudo systemctl"
-alias proxychains="proxychains -q"
-alias ag="ag --color --color-line-number '0;35' --color-match '46;30' --color-path '4;36'"
+# alias proxychains="proxychains -q"
+# alias ag="ag --color --color-line-number '0;35' --color-match '46;30' --color-path '4;36'"
 alias aga="ag --hidden --color --color-line-number '0;35' --color-match '46;30' --color-path '4;36'"
-alias tree='tree -CAFa -I "CVS|*.*.package|.svn|.git|.hg|node_modules|bower_components" --dirsfirst'
+# alias tree='tree -CAFa -I "CVS|*.*.package|.svn|.git|.hg|node_modules|bower_components" --dirsfirst'
 
 
 alias exzsh='exec zsh'
