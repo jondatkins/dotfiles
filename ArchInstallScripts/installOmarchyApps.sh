41ca14d62bebe92c13297c61efc39ec470d57db5
jondatkins <122027005+jondatkins@users.noreply.github.com>
2026-03-06 14:09:25 +0000

    Adding sesh.

--- ArchInstallScripts/installOmarchyApps.sh
+++ ArchInstallScripts/installOmarchyApps.sh
@@ -38,3 +38,4 @@ yay -S --needed --noconfirm wego
 yay -S --needed --noconfirm httpie
 sudo pacman --needed --noconfirm -S sqlc
 sudo pacman --needed --noconfirm -S television
+yay -S --needed --noconfirm sesh-bin
