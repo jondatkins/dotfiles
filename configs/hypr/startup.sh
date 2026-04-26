ce3718a94f98d86b877f9a92e0e4c1f272d391b2
jondatkins <122027005+jondatkins@users.noreply.github.com>
2026-04-26 12:24:16 +0100

    Starting tmux at startup.

--- configs/hypr/startup.sh
+++ configs/hypr/startup.sh
@@ -10,3 +10,16 @@ hyprctl dispatch workspace 1
 ghostty &
 chromium https://www.boot.dev/dashboard &
 firefox &
+tmux
+# tmux &
+# ~/dotfiles/configs/tmux/plugins/tmux-resurrect/scripts/restore.sh
+
+# tmux start-server
+#
+# # Wait for session to appear (max ~5 seconds)
+# for i in {1..10}; do
+#   tmux has-session -t mysession 2>/dev/null && break
+#   sleep 0.5
+# done
+#
+# tmux attach-session -t boot
