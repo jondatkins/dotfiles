2601d3d4b062e4f60ea070df495eaf0c532d9737
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2026-05-22 18:29:27 +0100

    Adding completion toggle bind to 'blink-cmp.lua', removing 'cmp.lua'. Completion source set to buffer, i.e. the current file.

--- configs/nvim-lazy/lua/plugins/blink-cmp.lua
+++ configs/nvim-lazy/lua/plugins/blink-cmp.lua
@@ -1,133 +1,169 @@
--- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/blink-cmp.lua
--- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/blink-cmp.lua
-
--- completion plugin with support for LSPs and external sources that updates
--- on every keystroke with minimal overhead
-
--- https://www.lazyvim.org/extras/coding/blink
--- https://github.com/saghen/blink.cmp
--- Documentation site: https://cmp.saghen.dev/
-
-if true then
-  return {}
-end
 return {
   "saghen/blink.cmp",
-  enabled = true,
-  opts = function(_, opts)
-    -- Merge custom sources with the existing ones from lazyvim
-    -- NOTE: by default lazyvim already includes the lazydev source, so not
-    -- adding it here again
-    opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-      default = { "lsp", "path", "snippets", "buffer", "luasnip" },
-      providers = {
-        lsp = {
-          name = "lsp",
-          enabled = true,
-          module = "blink.cmp.sources.lsp",
-          kind = "LSP",
-          -- When linking markdown notes, I would get snippets and text in the
-          -- suggestions, I want those to show only if there are no LSP
-          -- suggestions
-          fallbacks = { "snippets", "luasnip", "buffer" },
-          score_offset = 90, -- the higher the number, the higher the priority
-        },
-        luasnip = {
-          name = "luasnip",
-          enabled = true,
-          module = "blink.cmp.sources.luasnip",
-          min_keyword_length = 2,
-          fallbacks = { "snippets" },
-          score_offset = 85, -- the higher the number, the higher the priority
-        },
-        path = {
-          name = "Path",
-          module = "blink.cmp.sources.path",
-          score_offset = 3,
-          -- When typing a path, I would get snippets and text in the
-          -- suggestions, I want those to show only if there are no path
-          -- suggestions
-          fallbacks = { "snippets", "luasnip", "buffer" },
-          opts = {
-            trailing_slash = false,
-            label_trailing_slash = true,
-            get_cwd = function(context)
-              return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-            end,
-            show_hidden_files_by_default = true,
-          },
-        },
-        buffer = {
-          name = "Buffer",
-          module = "blink.cmp.sources.buffer",
-          min_keyword_length = 2,
-        },
-        snippets = {
-          name = "snippets",
-          enabled = true,
-          module = "blink.cmp.sources.snippets",
-          score_offset = 80, -- the higher the number, the higher the priority
-        },
-        -- Example on how to configure dadbod found in the main repo
-        -- https://github.com/kristijanhusak/vim-dadbod-completion
-        -- dadbod = {
-        --   name = "Dadbod",
-        --   module = "vim_dadbod_completion.blink",
-        --   score_offset = 85, -- the higher the number, the higher the priority
-        -- },
-        -- Third class citizen mf always talking shit
-        -- copilot = {
-        --   name = "copilot",
-        --   enabled = true,
-        --   module = "blink-cmp-copilot",
-        --   kind = "Copilot",
-        --   min_keyword_length = 2,
-        --   score_offset = 100, -- the higher the number, the higher the priority
-        --   async = true,
-        -- },
-      },
-    })
 
-    -- This comes from the luasnip extra, if you don't add it, won't be able to
-    -- jump forward or backward in luasnip snippets
-    -- https://www.lazyvim.org/extras/coding/luasnip#blinkcmp-optional
+  version = not vim.g.lazyvim_blink_main and "*",
+  build = vim.g.lazyvim_blink_main and "cargo build --release",
+  opts_extend = {
+    "sources.completion.enabled_providers",
+    "sources.compat",
+    "sources.default",
+  },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    -- add blink.compat to dependencies
+    {
+      "saghen/blink.compat",
+      optional = true, -- make optional so it's only enabled if any extras need it
+      opts = {},
+      version = not vim.g.lazyvim_blink_main and "*",
+    },
+  },
+  event = { "InsertEnter", "CmdlineEnter" },
+
+  opts = function(_, opts)
+    local completion_toggle = Snacks.toggle({
+      name = "Completion",
+      get = function()
+        return vim.b.completion
+      end,
+      set = function(state)
+        vim.b.completion = state
+      end,
+    })
+    local function toggle_completion()
+      require("blink.cmp").hide()
+      completion_toggle:toggle()
+    end
+    vim.keymap.set({ "i", "n" }, "<C-q>", toggle_completion, { desc = "Toggle Completion" })
+    opts.enabled = function()
+      return vim.b.completion
+    end
+
     opts.snippets = {
-      expand = function(snippet)
-        require("luasnip").lsp_expand(snippet)
-      end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require("luasnip").jumpable(filter.direction)
-        end
-        return require("luasnip").in_snippet()
-      end,
-      jump = function(direction)
-        require("luasnip").jump(direction)
-      end,
+      preset = "default",
     }
 
-    -- The default preset used by lazyvim accepts completions with enter
-    -- I don't like using enter because if on markdown and typing
-    -- something, but you want to go to the line below, if you press enter,
-    -- the completion will be accepted
-    -- https://cmp.saghen.dev/configuration/keymap.html#default
-    -- opts.keymap = {
-    --   preset = "default",
-    --   ["<Tab>"] = { "snippet_forward", "fallback" },
-    --   ["<S-Tab>"] = { "snippet_backward", "fallback" },
-    --
-    --   ["<Up>"] = { "select_prev", "fallback" },
-    --   ["<Down>"] = { "select_next", "fallback" },
-    --   ["<C-p>"] = { "select_prev", "fallback" },
-    --   ["<C-n>"] = { "select_next", "fallback" },
-    --
-    --   ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-    --   ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-    --
-    --   ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-    --   ["<C-e>"] = { "hide", "fallback" },
-    -- }
+    opts.appearance = {
+      use_nvim_cmp_as_default = false,
+      nerd_font_variant = "mono",
+    }
+
+    opts.completion = {
+      accept = {
+        auto_brackets = {
+          enabled = true,
+        },
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      ghost_text = {
+        enabled = vim.g.ai_cmp,
+      },
+    }
+
+    opts.sources = {
+      compat = {},
+      -- default = { "lsp", "path", "snippets", "buffer" },
+      default = { "buffer" },
+    }
+    opts.cmdline = {
+      enabled = true,
+      keymap = {
+        preset = "cmdline",
+        ["<Right>"] = false,
+        ["<Left>"] = false,
+      },
+      completion = {
+        list = { selection = { preselect = false } },
+        menu = {
+          auto_show = function(ctx)
+            return vim.fn.getcmdtype() == ":"
+          end,
+        },
+        ghost_text = { enabled = true },
+      },
+    }
+    opts.keymap = {
+      preset = "enter",
+      ["<C-y>"] = { "select_and_accept" },
+    }
 
     return opts
+  end,
+  -- MAKE CHANGE HERE
+  ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
+  config = function(_, opts)
+    if opts.snippets and opts.snippets.preset == "default" then
+      opts.snippets.expand = LazyVim.cmp.expand
+    end
+    -- setup compat sources
+    local enabled = opts.sources.default
+    for _, source in ipairs(opts.sources.compat or {}) do
+      opts.sources.providers[source] = vim.tbl_deep_extend(
+        "force",
+        { name = source, module = "blink.compat.source" },
+        opts.sources.providers[source] or {}
+      )
+      if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+        table.insert(enabled, source)
+      end
+    end
+
+    -- add ai_accept to <Tab> key
+    if not opts.keymap["<Tab>"] then
+      if opts.keymap.preset == "super-tab" then -- super-tab
+        opts.keymap["<Tab>"] = {
+          require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+          LazyVim.cmp.map({ "snippet_forward", "ai_nes", "ai_accept" }),
+          "fallback",
+        }
+      else -- other presets
+        opts.keymap["<Tab>"] = {
+          LazyVim.cmp.map({ "snippet_forward", "ai_nes", "ai_accept" }),
+          "fallback",
+        }
+      end
+    end
+
+    -- Unset custom prop to pass blink.cmp validation
+    opts.sources.compat = nil
+
+    -- check if we need to override symbol kinds
+    for _, provider in pairs(opts.sources.providers or {}) do
+      ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+      if provider.kind then
+        local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+        local kind_idx = #CompletionItemKind + 1
+
+        CompletionItemKind[kind_idx] = provider.kind
+        ---@diagnostic disable-next-line: no-unknown
+        CompletionItemKind[provider.kind] = kind_idx
+
+        ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+        local transform_items = provider.transform_items
+        ---@param ctx blink.cmp.Context
+        ---@param items blink.cmp.CompletionItem[]
+        provider.transform_items = function(ctx, items)
+          items = transform_items and transform_items(ctx, items) or items
+          for _, item in ipairs(items) do
+            item.kind = kind_idx or item.kind
+            item.kind_icon = LazyVim.config.icons.kinds[item.kind_name] or item.kind_icon or nil
+          end
+          return items
+        end
+
+        -- Unset custom prop to pass blink.cmp validation
+        provider.kind = nil
+      end
+    end
+
+    require("blink.cmp").setup(opts)
   end,
 }
