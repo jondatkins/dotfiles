0878291eac18518808a3185576122db41c3d4822
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2026-07-23 14:17:52 +0100

    Adding a sesh file. Commenting out some error causing zshrc config. kanata and script changes.

--- configs/scripts/udemy.sh
+++ configs/scripts/udemy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-firefox https://www.udemy.com/course/100-days-of-code/?couponCode=KEEPLEARNING &
+firefox --new-window https://www.udemy.com/course/100-days-of-code/?couponCode=KEEPLEARNING &
 exec ghostty -e zsh -lc 'tmuxinator udemy'
