d5bcef72ff43bab475463deec0024c3fda201659
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2026-08-25 22:06:50 +0100

    Script to start game dev book study.

---
+++ configs/scripts/gameDev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+evince ~/Local_Books/Game_Programming/beginningcplusplusgameprogramming.pdf &
+ghostty -e sesh connect ~/Projects/BeginGameProg/
+firefox https://www.sfml-dev.org/tutorials/3.1/migration/sfml-2/
