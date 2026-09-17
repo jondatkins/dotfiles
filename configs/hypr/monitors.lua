ede745e40d7e3024671158988e5eec8da452fc63
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2026-09-17 14:26:38 +0100

    Mapping tablet to monitor

--- configs/hypr/monitors.lua
+++ configs/hypr/monitors.lua
@@ -1,15 +1,15 @@
 -- Personal monitorxt
 
 hl.monitor({
-  output = "HDMI-A-1",
-  mode = "1920x1080@60",
-  position = "0x0",
-  scale = 1,
+	output = "HDMI-A-1",
+	mode = "1920x1080@60",
+	position = "0x0",
+	scale = 1,
 })
 
 hl.monitor({
-  output = "DVI-D-1",
-  mode = "1920x1080@60",
-  position = "0x1080",
-  scale = 1,
+	output = "DVI-D-1",
+	mode = "1920x1080@60",
+	position = "0x1080",
+	scale = 1,
 })
