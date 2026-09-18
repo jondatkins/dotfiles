c1f25761b1fd510616b5dbcab4007e5636f9add4
jonathan-atkins <122027005+jondatkins@users.noreply.github.com>
2026-09-18 20:35:59 +0100

    adding vial for keyboard config

--- ArchInstallScripts/installOmarchyApps.sh
+++ ArchInstallScripts/installOmarchyApps.sh
@@ -47,4 +47,5 @@ sudo pacman --needed --noconfirm -S gamescope
 sudo pacman --needed --noconfirm -S emacs
 sudo pacman --needed --noconfirm -S steam
 yay -S --needed --noconfirm tmuxinator
+yay -S --needed --noconfirm vial-appimage
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
